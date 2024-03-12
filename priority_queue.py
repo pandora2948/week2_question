31cb7e6a732a3065415b80a4d7fb3358152da621
백동재 <baekdongjae@baegdongjaeui-MacBookPro.local>
2024-03-12 11:56:59 +0900

    refactor: fix Priority Queue logic and refactor _shift_down function

--- priority_queue.py
+++ priority_queue.py
@@ -10,20 +10,20 @@ class PriorityQueue:
   def pop(self):
     if len(self.heap) > 1:
       self._swap(0, len(self.heap) - 1)
-      item = self.heap.pop()
+      _, item = self.heap.pop()
       self._sift_down(0)
-      return item[1]
+      return item
 
     elif len(self.heap) == 1:
-      item = self.heap.pop()
-      return item[1]
+      _, item = self.heap.pop()
+      return item
 
     else:
       return None
 
   def _sift_up(self, index):
     while index > 0:
-      parent_index = index // 2
+      parent_index = (index - 1) // 2
 
       if self.heap[parent_index][0] > self.heap[index][0]:
         self._swap(parent_index, index)
@@ -34,16 +34,16 @@ class PriorityQueue:
 
   def _sift_down(self, index):
     while True:
-      left_child_index = 2 * index
-      right_child_index = 2 * index + 1
+      left_child_index = 2 * index + 1
+      right_child_index = 2 * index + 2
       smallest = index
 
       if left_child_index < len(self.heap) and \
-        self.heap[left_child_index][0] > self.heap[smallest][0]:
+          self.heap[left_child_index][0] < self.heap[smallest][0]:
         smallest = left_child_index
 
       if right_child_index < len(self.heap) and \
-        self.heap[right_child_index][0] > self.heap[smallest][0]:
+          self.heap[right_child_index][0] < self.heap[smallest][0]:
         smallest = right_child_index
 
       if smallest != index:
